c11c4997ce9954f6b01690fbffa2760f74866432
Luis Miguel Garcia <Gordisandroid@gmail.com>
2024-07-07 11:32:09 -0400

    migrations db

--- Prime/foso/models.py
+++ Prime/foso/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
+#club se refiere al equipo del jugador en el torneo
 # Create your models here.
+class Jugadores(models.Model):
+    nombre = models.CharField(max_length=100)
+    posicion = models.CharField(max_length=100)
+    club = models.CharField(max_length=100)
+    goles = models.IntegerField()
+
--- Prime/jugadores/models.py
+++ Prime/jugadores/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
+
 # Create your models here.
+class players(models.Model):
+    nombre = models.CharField(max_length=100)
+    finales = models.IntegerField()
+    finales_Ganadas = models.IntegerField()
